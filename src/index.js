3b57e388a4edf66785ad23b85bcfcf3fe41654e7
TayTufenkjian <taytuf@gmail.com>
2022-06-07 18:29:29 -0400

    Clear form after adding a to-do

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import './style.css';
 import {toDo, addToDo, showToDoForm, showToDos} from './todo.js';
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    showToDos();
+    listenForNewToDo();
+});
+
 
 function listenForNewToDo() {
     let btnAdd = document.getElementById('add-button');
@@ -10,15 +15,16 @@ function listenForNewToDo() {
         let dueDate = document.getElementById('due-date').value;
         let priority = document.getElementById('priority').value;
         let description = document.getElementById('description').value;
-
         let complete = false;
-
+        
         let newToDo = toDo(title, complete, dueDate, priority, description);
         
         addToDo(newToDo);
 
         showToDos();
+
+        // Clear the form
+        document.getElementById('add-toDo').reset();
     })
   }
-
-  listenForNewToDo();
+  
