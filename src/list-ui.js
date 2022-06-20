e352df307acf89a76d2b0cb2b6a5f7962ad17899
TayTufenkjian <taytuf@gmail.com>
2022-06-20 15:58:44 -0400

    Lint some of the list UI module
    Update the low-hanging fruit
    but leave dependency cycle for a separate effort

--- src/list-ui.js
+++ src/list-ui.js
@@ -1,99 +1,101 @@
-import {createFormField, clearPageContent, createPageHeader, createButtonInDiv, updateActiveNavItem, createButton} from './ui-helpers.js';
-import {getLists, addList, getListItems, getListName, deleteList} from './list.js';
-import {showAddToDoForm, showToDos} from './todo-ui.js';
+import {
+  createFormField, clearPageContent, createPageHeader, createButtonInDiv,
+  updateActiveNavItem, createButton,
+} from './ui-helpers';
 
+import {
+  getLists, addList, getListItems, getListName, deleteList,
+} from './list';
 
-let main = document.querySelector('main');
+import { showAddToDoForm, showToDos } from './todo-ui';
 
-function listenForAddNewList() {
-    document.getElementById('add-list').addEventListener('click', () => {
-        clearPageContent();
-        let header = createPageHeader('Add New List');
-        
-        let form = document.createElement('form');
-        form.id = 'add-new-list';
-
-        let listNameField = createFormField('text', 'list-name', 'list-name', 'Name of list', true);
-        
-        let btnAdd = document.createElement('input');
-        btnAdd.type = 'submit';
-        btnAdd.id = 'add-list';
-        btnAdd.classList.add('primary');
-        btnAdd.value = 'Add list';
-
-        form.append(listNameField, btnAdd);
-
-        form.onsubmit = () => {
-            let listName = document.getElementById('list-name').value;
-            addList(listName);
-            showLists();
-            updateActiveNavItem('show-lists');
-            // Prevent the form from actually submitting
-            return false;
-        }
-        
-        main.append(header, form);
-    });
-}
-
+const main = document.querySelector('main');
 
 function showLists() {
-    clearPageContent();
-    let header = createPageHeader('Your Lists');
-    let unorderedList = document.createElement('ul');
-    unorderedList.classList.add('lists');
+  clearPageContent();
+  const header = createPageHeader('Your Lists');
+  const unorderedList = document.createElement('ul');
+  unorderedList.classList.add('lists');
 
-    // Skip the default list when displaying the user's lists
-    let userLists = getLists().slice(1);
-    for (let list of userLists) {
-        let listElement = document.createElement('li');
-        listElement.id = list.id;
-        listElement.textContent = list.name;
-        listElement.addEventListener('click', () => {
-            showListItems(list.id);
-        });
-        unorderedList.append(listElement);
-    }
+  // Skip the default list when displaying the user's lists
+  const userLists = getLists().slice(1);
+  userLists.forEach((list) => {
+    const listElement = document.createElement('li');
+    listElement.id = list.id;
+    listElement.textContent = list.name;
+    listElement.addEventListener('click', () => {
+      showListItems(list.id);
+    });
+    unorderedList.append(listElement);
+  });
 
-    main.append(header, unorderedList);
+  main.append(header, unorderedList);
 }
-
 
 function listenForShowLists() {
-    document.getElementById('show-lists').addEventListener('click', () => {       
-       showLists();
-    });
+  document.getElementById('show-lists').addEventListener('click', () => {
+    showLists();
+  });
 }
-
 
 function showListItems(listID) {
-    clearPageContent();
+  clearPageContent();
 
-    let listName = getListName(listID);
-    let header = createPageHeader(listName);
-    header.id = listID;
+  const listName = getListName(listID);
+  const header = createPageHeader(listName);
+  header.id = listID;
 
-    let listItems = getListItems(listID);
-    let listItemsDiv = showToDos(listItems, listID);    
+  const listItems = getListItems(listID);
+  const listItemsDiv = showToDos(listItems, listID);
 
-    let addFunction = () => {
-        showAddToDoForm(listID);
+  const addFunction = () => {
+    showAddToDoForm(listID);
+  };
+  const btnDiv = createButtonInDiv('Add new', addFunction, 'primary');
+  btnDiv.classList.add('list', 'options');
+
+  const deleteFunction = () => {
+    const confirmDelete = confirm('Do you really want to delete this list? You will also delete all to-do items in the list.');
+    if (confirmDelete) {
+      deleteList(listID);
+      showLists();
     }
-    let btnDiv = createButtonInDiv('Add new', addFunction, 'primary');
-    btnDiv.classList.add('list','options');
+  };
+  const btnDeleteList = createButton('Delete list', deleteFunction, 'secondary');
+  btnDiv.append(btnDeleteList);
 
-    let deleteFunction = () => {
-        let confirmDelete = confirm('Do you really want to delete this list? You will also delete all to-do items in the list.');
-        if (confirmDelete) {
-            deleteList(listID);
-            showLists();
-        }
-    }
-    let btnDeleteList = createButton('Delete list', deleteFunction, 'secondary')
-    btnDiv.append(btnDeleteList);
-
-    main.append(header, listItemsDiv, btnDiv);
+  main.append(header, listItemsDiv, btnDiv);
 }
 
+function listenForAddNewList() {
+  document.getElementById('add-list').addEventListener('click', () => {
+    clearPageContent();
+    const header = createPageHeader('Add New List');
 
-export {listenForAddNewList, listenForShowLists, showListItems};
+    const form = document.createElement('form');
+    form.id = 'add-new-list';
+
+    const listNameField = createFormField('text', 'list-name', 'list-name', 'Name of list', true);
+
+    const btnAdd = document.createElement('input');
+    btnAdd.type = 'submit';
+    btnAdd.id = 'add-list';
+    btnAdd.classList.add('primary');
+    btnAdd.value = 'Add list';
+
+    form.append(listNameField, btnAdd);
+
+    form.onsubmit = () => {
+      const listName = document.getElementById('list-name').value;
+      addList(listName);
+      showLists();
+      updateActiveNavItem('show-lists');
+      // Prevent the form from actually submitting
+      return false;
+    };
+
+    main.append(header, form);
+  });
+}
+
+export { listenForAddNewList, listenForShowLists, showListItems };
